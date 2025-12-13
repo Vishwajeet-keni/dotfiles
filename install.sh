dd691b7a2760c9f9da6b67a73527edf9effe0a70
Vishwajeet Keni <kenivishwajeet874@gmail.com>
2025-12-13 13:00:30 +0530

    install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DOTFILES="$HOME/dotfiles"
+
+ln -sf "$DOTFILES/.config/nvim" "$HOME/.config/nvim"
+ln -sf "$DOTFILES/.config/hypr" "$HOME/.config/hypr"
+ln -sf "$DOTFILES/.config/eww" "$HOME/.config/eww"
+ln -sf "$DOTFILES/.config/scripts" "$HOME/.config/scripts"   
