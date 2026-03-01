e9bc32e65e986baaef0c0d377ffccc50dcd26b2d
Vishwajeet Keni <kenivishwajeet874@gmail.com>
2026-03-01 14:30:24 +0530

    switching to ncvhad

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,35 +1,37 @@
-require 'core.options' -- Load general options
-require 'core.keymaps' -- Load general keymaps
-require 'core.snippets'
---require 'core.snippets' -- Custom code snippets
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.mapleader = " "
 
--- Set up the Lazy plugin manager
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
+
 vim.opt.rtp:prepend(lazypath)
 
--- Set up plugins
-require('lazy').setup {
-  require("plugins.neotree"),
-  require("plugins.colorTheme"),
-  require("plugins.bufferline"),
-  require("plugins.lualine"),
-  require("plugins.treesitter"),
-  require("plugins.telescope"),
-  require("plugins.lsp"),
-  require("plugins.autocompletion"),
-  require("plugins.gitsings"),
-  require("plugins.alpha"),
-  require("plugins.indent-blankline"),
-  require("plugins.misc"),
-  require("plugins.comment"),
-  require("plugins.runcode"),
-  require("plugins.arduino")
-}
+local lazy_config = require "configs.lazy"
 
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "options"
+require "autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
