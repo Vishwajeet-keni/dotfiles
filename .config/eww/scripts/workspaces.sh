c22992835d9f904c7e9334c2d3ddcc47f089ca2f
Vishwajeet Keni <kenivishwajeet874@gmail.com>
2026-03-06 10:49:40 +0530

    added workspaces

--- .config/eww/scripts/workspaces.sh
+++ .config/eww/scripts/workspaces.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-hyprctl workspaces -j | jq -r '.[] | "\(.id) \(.windows)"'   
+
+# Get current workspace from hyprctl (without jq)
+current_workspace=$(hyprctl activeworkspace | grep "ID" | awk '{print $3}')
+
+# Get all workspaces (without jq)
+workspaces=$(hyprctl workspaces | grep "ID" | awk '{print $3}' | sort -n | tr '\n' ' ')
+
+# Format as JSON manually
+workspaces_clean=$(echo $workspaces | xargs | tr ' ' ',')
+echo "{\"current\": $current_workspace, \"workspaces\": [$workspaces_clean]}"
