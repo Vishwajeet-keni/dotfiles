8e4feb8fc0f0754856b3f5d241691e35fd26b8df
Vishwajeet Keni <kenivishwajeet874@gmail.com>
2026-03-06 14:41:47 +0530

    making workspaces dynamic

--- .config/eww/scripts/workspaces.sh
+++ .config/eww/scripts/workspaces.sh
@@ -6,6 +6,5 @@ current_workspace=$(hyprctl activeworkspace | grep "ID" | awk '{print $3}')
 # Get all workspaces (without jq)
 workspaces=$(hyprctl workspaces | grep "ID" | awk '{print $3}' | sort -n | tr '\n' ' ')
 
-# Format as JSON manually
-workspaces_clean=$(echo $workspaces | xargs | tr ' ' ',')
-echo "{\"current\": $current_workspace, \"workspaces\": [$workspaces_clean]}"
+all_workspaces=$(echo "1 2 3 4 5 $workspaces" | tr ' ' '\n' | grep -v '^$' | sort -nu | tr '\n' ',' | sed 's/,$//')
+echo "{\"current\": $current_workspace, \"workspaces\": [$all_workspaces]}"
