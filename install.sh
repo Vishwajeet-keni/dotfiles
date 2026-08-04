a04e97570ce2e621dad26687ff4d5d0e5b68665c
Vishwajeet Keni <kenivishwajeet874@gmail.com>
2026-08-04 17:02:17 +0530

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 DOTFILES="$HOME/dotfiles"
 
+mkdir -p "$HOME/.config"
+
 ln -sf "$DOTFILES/.config/nvim" "$HOME/.config/nvim"
 ln -sf "$DOTFILES/.config/hypr" "$HOME/.config/hypr"
 ln -sf "$DOTFILES/.config/eww" "$HOME/.config/eww"
-ln -sf "$DOTFILES/.config/scripts" "$HOME/.config/scripts"   
+ln -sf "$DOTFILES/.config/scripts" "$HOME/.config/scripts"
+ln -sf "$DOTFILES/.config/gtk-3.0" "$HOME/.config/gtk-3.0"
+ln -sf "$DOTFILES/.config/gtk-4.0" "$HOME/.config/gtk-4.0"
+ln -sf "$DOTFILES/.config/kitty" "$HOME/.config/kitty"
+ln -sf "$DOTFILES/.config/rofi" "$HOME/.config/rofi"
 ln -sf "$DOTFILES/wallpapers" "$HOME/wallpapers"
