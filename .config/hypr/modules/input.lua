8e349df358d788cb947aaea66976f0095aaad0f2
Vishwajeet Keni <kenivishwajeet874@gmail.com>
2026-08-08 12:05:12 +0530

    +1 keybind change

--- .config/hypr/modules/input.lua
+++ .config/hypr/modules/input.lua
@@ -22,7 +22,7 @@ hl.config({
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Gestures/
 hl.gesture({ fingers = 3, direction = "horizontal", action = "workspace" })
-hl.gesture({ fingers = 4, direction = "down", action = "close" })
+hl.gesture({ fingers = 3, direction = "down", action = "close" })
 -- Example per-device config
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
 hl.device({ name = "epic-mouse-v1", sensitivity = -0.5 })
